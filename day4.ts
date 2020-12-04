658e14575aa332335dfc46b477451082db18d38d
Minna Niemi <35032643+minnacaptain@users.noreply.github.com>
2020-12-04 09:33:22 +0200

    Add day4

---
+++ day4.ts
@@ -0,0 +1,72 @@
+const decoder = new TextDecoder("utf-8");
+const input = decoder.decode(Deno.readFileSync("./day4_input"));
+const rows = input.split(`
+`);
+let passports = [] as string[];
+let current = "" as string;
+rows.forEach((row) => {
+  if (row !== "") {
+    current = current + " " + row;
+  } else {
+    passports = [...passports, current];
+    current = "";
+  }
+});
+passports = [...passports, current];
+
+const between = (min: number, max: number) => (x: string) =>
+  Number(x) >= min && Number(x) <= max;
+
+const validateHairColor = (x: string) =>
+  Boolean(x.match(/^#{1}[a-f0-9]{6}$/)?.length);
+
+const validateEyeColor = (x: string) =>
+  ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(x);
+
+const validatePassportId = (x: string) =>
+  Boolean(x.match(/^[0-9]{9}$/)?.length);
+
+const validateHeight = (x: string) =>
+  (x.endsWith("cm") && between(150, 193)(x.replace("cm", ""))) ||
+  (x.endsWith("in") && between(59, 76)(x.replace("in", "")));
+
+const validationRules = {
+  byr: between(1920, 2002),
+  iyr: between(2010, 2020),
+  eyr: between(2020, 2030),
+  hgt: validateHeight,
+  hcl: validateHairColor,
+  ecl: validateEyeColor,
+  pid: validatePassportId,
+};
+
+// Part 1
+const requiredFields = Object.keys(validationRules);
+const passportsWithAllRequiredFields = passports.filter(
+  (p) =>
+    p
+      .split(" ")
+      .filter((x) => x != "")
+      .filter((p) => requiredFields.includes(p.substr(0, 3))).length ===
+    requiredFields.length
+);
+console.log("Part 1: ", passportsWithAllRequiredFields.length);
+
+// Part 2
+const passportsPassingAllValidation = passportsWithAllRequiredFields.filter(
+  (p) =>
+    p
+      .split(" ")
+      .filter((x) => x != "")
+      .map((x) => x.split(":"))
+      .map(
+        (x) =>
+          x[0] === "cid" ||
+          Boolean(
+            (validationRules as any)[x[0]] &&
+              (validationRules as any)[x[0]](x[1])
+          )
+      )
+      .reduce((a, c) => a && c)
+);
+console.log("Part 2: ", passportsPassingAllValidation.length);
